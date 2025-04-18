2c5bbb13f65a7c73b904e5885accdd8327c9b5ec
Geo Galagaran <17492631+geeofree@users.noreply.github.com>
2025-04-18 15:28:05 +0800

    chore: add proxy to dev server for api endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import tsConfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsConfigPaths()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8787",
+        changeOrigin: true,
+      },
+    },
+  },
 });
